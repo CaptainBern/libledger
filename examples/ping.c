f0b867a419536f4739a346151d5a687a10dab4a9
CaptainBern <bengt@verscheu.re>
2018-03-01 05:39:41 +0100

    Added more example function calls

--- examples/ping.c
+++ examples/ping.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <hidapi/hidapi.h>
 #include <libledger/libledger.h>
@@ -24,6 +25,7 @@ int main(int argc, char *argv[]) {
 
 	hid_free_enumeration(dev_info);
 
+	// Ping the device
 	printf("Pinging device...\n");
 
 	ret = ledger_transport_ping(device);
@@ -32,10 +34,23 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	printf("Ping success\n");
+	printf("\tPing success\n");
 
+	// Allocate a communication channel
+	printf("Allocating Channel:\n");
+
+	struct ledger_transport_channel channel;
+	ret = ledger_transport_allocate_channel(device, &channel);
+	if (ret != LEDGER_SUCCESS) {
+		printf("Failed to allocate channel %s\n", ledger_error_string(ret));
+		return EXIT_FAILURE;
+	}
+
+	printf("\tChannel ID: 0x%x\n", channel.channel_id);
+
+	// Request the BOLOS version
 	struct ledger_bolos_version *version;
-	ret = ledger_bolos_get_version(device, &version);
+	ret = ledger_bolos_get_version(device, channel.channel_id, &version);
 	if (ret != LEDGER_SUCCESS) {
 		printf("Failed to retrieve the BOLOS version info: %s\n", ledger_error_string(ret));
 		return EXIT_FAILURE;
@@ -46,9 +61,33 @@ int main(int argc, char *argv[]) {
 	printf("\tOS version: %s\n", version->os_version);
 	printf("\tMCU version: %s\n", version->mcu_version);
 
+	// Validate target ID
+	printf("Validating target ID:\n");
+	ret = ledger_bolos_validate_target_id(device, channel.channel_id, version->target_id);
+	if (ret != LEDGER_SUCCESS) {
+		printf("Failed to validate target ID: %s\n", ledger_error_string(ret));
+		return EXIT_FAILURE;
+	}
+
+	printf("\tTarget ID validated\n");
+
 	ledger_bolos_free_version(version);
 
+	/*
+	printf("Resetting device:\n");
+
+	ret = ledger_bolos_reset(device, channel.channel_id);
+	if (ret != LEDGER_SUCCESS) {
+		printf("Failed to reset device: %s\n", ledger_error_string(ret));
+		return EXIT_FAILURE;
+	}
+
+	printf("\tReset success\n");
+	*/
+
 	ledger_close_device(device);
+
+	hid_exit();
 
 	return EXIT_SUCCESS;
 }
