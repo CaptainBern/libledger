d9acc7a9089372eb240148fad0274d994a958637
Bengt Verscheure <git@verscheu.re>
2018-04-10 00:02:36 +0200

    Version

--- bolos/version.c
+++ bolos/version.c
@@ -36,112 +36,102 @@ bool ledger_bolos_get_version(struct ledger_device *device, uint16_t channel_id,
 	if (!ledger_apdu_exchange(device, channel_id, &command, &reply))
 		return false;
 
-	if (reply->status != 0x9000)
+	if (reply->status != 0x9000) {
+		LEDGER_SET_ERROR(device, LEDGER_ERROR_OTHER);
 		goto err_destroy_apdu_reply;
+	}
 
 	struct ledger_cursor cursor;
 	ledger_cursor_init(&cursor, reply->data->data, reply->data->len);
 
-	uint32_t target_id = 0;
-	if (!ledger_cursor_read_u32(&cursor, &target_id)) {
-		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
+	struct ledger_bolos_version *_version = calloc(1, sizeof(struct ledger_bolos_version));
+	if (!_version) {
+		LEDGER_SET_ERROR(device, LEDGER_ERROR_NOMEM);
 		goto err_destroy_apdu_reply;
+	}
+
+	if (!ledger_cursor_read_u32(&cursor, &_version->target_id)) {
+		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
+		goto err_destroy_version;
 	}
 
 	uint8_t os_version_len = 0;
 	if (!ledger_cursor_read_u8(&cursor, &os_version_len)) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
-		goto err_destroy_apdu_reply;
+		goto err_destroy_version;
 	}
 
-	char *os_version = calloc(os_version_len, sizeof(char));
-	if (!os_version) {
+	_version->os_version = calloc(os_version_len, sizeof(char));
+	if (!_version->os_version) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_NOMEM);
-		goto err_destroy_apdu_reply;
+		goto err_destroy_version;
 	}
 
-	if (!ledger_cursor_read_bytes(&cursor, (uint8_t *) os_version, os_version_len)) {
+	if (!ledger_cursor_read_bytes(&cursor, (uint8_t *) _version->os_version, os_version_len)) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
-		goto err_destroy_os_version;
+		goto err_destroy_version;
 	}
 
-	uint8_t flags_len = 0;
-	if (!ledger_cursor_read_u8(&cursor, &flags_len)) {
+	if (!ledger_cursor_read_u8(&cursor, &_version->flags.len)) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
-		goto err_destroy_os_version;
+		goto err_destroy_version;
 	}
 
-	uint8_t *flags = calloc(flags_len, sizeof(uint8_t));
-	if (!flags) {
+	_version->flags.flags = calloc(_version->flags.len, sizeof(uint8_t));
+	if (!_version->flags.flags) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_NOMEM);
-		goto err_destroy_os_version;
+		goto err_destroy_version;
 	}
 
-	if (!ledger_cursor_read_bytes(&cursor, flags, flags_len)) {
+	if (!ledger_cursor_read_bytes(&cursor, _version->flags.flags, _version->flags.len)) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
-		goto err_destroy_flags;
+		goto err_destroy_version;
 	}
 
 	uint8_t mcu_version_len = 0;
 	if (!ledger_cursor_read_u8(&cursor, &mcu_version_len)) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
-		goto err_destroy_flags;
+		goto err_destroy_version;
 	}
 
-	char *mcu_version = calloc(mcu_version_len, sizeof(char));
-	if (!mcu_version) {
+	_version->mcu_version = calloc(mcu_version_len, sizeof(char));
+	if (!_version->mcu_version) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_NOMEM);
-		goto err_destroy_flags;
+		goto err_destroy_version;
 	}
 
-	if (!ledger_cursor_read_bytes(&cursor, (uint8_t *) mcu_version, mcu_version_len)) {
+	if (!ledger_cursor_read_bytes(&cursor, (uint8_t *) _version->mcu_version, mcu_version_len)) {
 		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
-		goto err_destroy_mcu_version;
+		goto err_destroy_version;
 	}
 
-	uint8_t mcu_hash_len = 0;
-	if (!ledger_cursor_read_u8(&cursor, &mcu_hash_len)) {
-		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
-		goto err_destroy_mcu_version;
+	// Only target ID 0x31100003 sends the MCU hash
+	if ((_version->target_id & 0xff) == 0x03) {
+		if (!ledger_cursor_read_u8(&cursor, &_version->mcu_hash.len)) {
+			LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
+			goto err_destroy_version;
+		}
+
+		_version->mcu_hash.hash = calloc(_version->mcu_hash.len, sizeof(uint8_t));
+		if (!_version->mcu_hash.hash) {
+			LEDGER_SET_ERROR(device, LEDGER_ERROR_NOMEM);
+			goto err_destroy_version;
+		}
+
+		if (!ledger_cursor_read_bytes(&cursor, _version->mcu_hash.hash, _version->mcu_hash.len)) {
+			LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
+			goto err_destroy_version;
+		}
 	}
 
-	uint8_t *mcu_hash = calloc(mcu_hash_len, sizeof(uint8_t));
-	if (!mcu_hash) {
-		LEDGER_SET_ERROR(device, LEDGER_ERROR_NOMEM);
-		goto err_destroy_mcu_version;
-	}
-
-	if (!ledger_cursor_read_bytes(&cursor, mcu_hash, mcu_hash_len)) {
-		LEDGER_SET_ERROR(device, LEDGER_ERROR_IO);
-		goto err_destroy_mcu_hash;
-	}
-
-	struct ledger_bolos_version *_version = malloc(sizeof(struct ledger_bolos_version));
-	if (!_version) {
-		LEDGER_SET_ERROR(device, LEDGER_ERROR_NOMEM);
-		goto err_destroy_mcu_hash;
-	}
-
-	_version->target_id = target_id;
-	_version->os_version = os_version;
-	_version->flags.len = flags_len;
-	_version->flags.flags = flags;
-	_version->mcu_version = mcu_version;
-	_version->mcu_hash.len = mcu_hash_len;
-	_version->mcu_hash.hash = mcu_hash;
+	ledger_apdu_reply_destroy(reply);
 
 	*version = _version;
 
 	return true;
 
-err_destroy_mcu_hash:
-	free(mcu_hash);
-err_destroy_mcu_version:
-	free(mcu_version);
-err_destroy_flags:
-	free(flags);
-err_destroy_os_version:
-	free(os_version);
+err_destroy_version:
+	ledger_bolos_version_destroy(_version);
 err_destroy_apdu_reply:
 	ledger_apdu_reply_destroy(reply);
 	return false;
